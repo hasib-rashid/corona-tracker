11adf3df1ae67fc7f9d6c5cda2feb75406e90845
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-18 15:45:45 +0600

    👑Map Done

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+// @ts-ignore
+import { VectorMap } from "react-jvectormap";
+import "./map.css";
+import { useWindowSize } from "../../hooks/useWindowSize";
+
+export default function Map(props: any) {
+    return (
+        <div className="map-container">
+            <VectorMap
+                map={"world_mill"}
+                backgroundColor="transparent" //change it to ocean blue: #0077be
+                zoomOnScroll={false}
+                containerStyle={{
+                    width: "100%",
+                    height: "400px",
+                }}
+                onRegionClick={props.handleClick} //gets the country code
+                containerClassName="map"
+                regionStyle={{
+                    initial: {
+                        fill: "#e4e4e4",
+                        "fill-opacity": 0.9,
+                        stroke: "none",
+                        "stroke-width": 0,
+                        "stroke-opacity": 0,
+                    },
+                    hover: {
+                        "fill-opacity": 0.8,
+                        cursor: "pointer",
+                    },
+                    selected: {
+                        fill: "#2938bc", //color for the clicked country
+                    },
+                    selectedHover: {},
+                }}
+                regionsSelectable={false}
+                onRegionTipShow={(e: any, el: any, code: any) => {
+                    const data = props.mapData[code];
+                    return el.html(
+                        "<b>" +
+                        el.html() +
+                        "</b></br>" +
+                        "<b> confirmed cases: </b>" +
+                        (data ? data : 0)
+                    );
+                }}
+                series={{
+                    regions: [
+                        {
+                            values: props.mapData, //this is your data
+                            scale: ["#FEE5D9", "#A50F15"], //your color game's here
+                            normalizeFunction: "polynomial",
+                        },
+                    ],
+                }}
+            />
+            {/* <div className="title">Number Of Confirmed Cases All Over The World</div> */}
+        </div>
+    );
+}
