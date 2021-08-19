7ebbd2ca2dcfab5675172664346a0fd54a7a5b9c
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-08-19 15:41:32 +0600

    💕useState

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Information.css'
 
 const Information = () => {
+    const [response, setResponse]: any = useState({})
+
     useEffect(() => {
         const fetching = async () => {
             const res = await fetch("https://disease.sh/v3/covid-19/all/")
             const resJson = await res.json()
 
-            console.log(resJson)
+            setResponse(resJson)
         }
 
         fetching()
@@ -17,7 +19,7 @@ const Information = () => {
         <article>
             <h2>WorldWide</h2>
             <div className="info_row">
-                <h3>Confirmed: <span className="confirmed">32498327874327</span></h3>
+                <h3>Confirmed: <span className="confirmed">{response.cases}</span></h3>
                 <h3>Deaths: <span className="deaths">94832842</span></h3>
                 <h3>Recovered: <span className="recovered">4328748242080</span></h3>
             </div>
